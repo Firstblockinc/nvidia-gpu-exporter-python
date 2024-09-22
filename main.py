16c60f886ac09592a40742a7c36d1bc02163ddba
jayslachance <ja_sonlachance@hotmail.com>
2024-09-22 14:12:50 -0400

    main.py missing objects management

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from prometheus_client import start_http_server
 import time
-from poller import poll_all_rigs
-from config_loader import load_config
+from src.poller import poll_all_rigs
+from src.config_loader import load_config
 
+##setup rig objects and provisiong next
 if __name__ == "__main__":
     config = load_config("config.json")
     rigs = config["rigs"]
 
-    # Start Prometheus HTTP server on port 8000
     start_http_server(8000)
 
-    # Poll each rig every 60 seconds
     while True:
         poll_all_rigs(rigs)
-        time.sleep(60)
+        time.sleep(300)
