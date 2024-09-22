7030284d47e276e6e729046fada956c13c5b1bd2
jayslachance <ja_sonlachance@hotmail.com>
2024-09-22 14:12:57 -0400

    modified metrics to collect

--- src/metrics.py
+++ src/metrics.py
@@ -1,8 +1,8 @@
 from prometheus_client import Gauge
 
-# Define Prometheus metrics for each attribute we want to collect
-gpu_utilization_metric = Gauge('gpu_utilization', 'GPU Utilization', ['rig', 'gpu'])
-gpu_memory_total_metric = Gauge('gpu_memory_total', 'GPU Memory Total (MB)', ['rig', 'gpu'])
-gpu_memory_free_metric = Gauge('gpu_memory_free', 'GPU Memory Free (MB)', ['rig', 'gpu'])
-gpu_memory_used_metric = Gauge('gpu_memory_used', 'GPU Memory Used (MB)', ['rig', 'gpu'])
-gpu_temperature_metric = Gauge('gpu_temperature', 'GPU Temperature (°C)', ['rig', 'gpu'])
+gpu_utilization_metric = Gauge('gpu_utilization', 'GPU Utilization', ['rig', "ip", 'gpu_model', "gpu_id"])
+gpu_memory_total_metric = Gauge('gpu_memory_total', 'GPU Memory Total (MB)', ['rig', "ip", 'gpu_model', "gpu_id"])
+gpu_memory_free_metric = Gauge('gpu_memory_free', 'GPU Memory Free (MB)', ['rig', "ip", 'gpu_model', "gpu_id"])
+gpu_memory_used_metric = Gauge('gpu_memory_used', 'GPU Memory Used (MB)', ['rig', "ip", 'gpu_model', "gpu_id"])
+gpu_temperature_metric = Gauge('gpu_temperature', 'GPU Temperature (°C)', ['rig', "ip", 'gpu_model', "gpu_id"])
+gpu_power_consumption_metric = Gauge('gpu_power_consumption', 'GPU Power Consumption (W)', ['rig', 'ip', 'gpu_model', 'gpu_id'])
